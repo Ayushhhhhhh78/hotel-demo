4ebd154f3ea522c8688d51f2a154823a9f75e1c6
Ayushhhhhhh78 <uvaayushyt@gmail.com>
2026-02-23 23:31:12 +0530

    binding server globally

--- app.js
+++ app.js
@@ -207,6 +207,6 @@ app.use((req, res) => {
 // START SERVER
 // =====================
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0" , () => {
     console.log(`🏨 DEMO HOTEL server running at http://localhost:${PORT}`);
 });
